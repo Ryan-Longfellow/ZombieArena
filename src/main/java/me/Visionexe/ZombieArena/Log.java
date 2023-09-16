0986023a25d59c588918670bb5d8d0e9044a9ce6
Ryan-Longfellow <30578561+Ryan-Longfellow@users.noreply.github.com>
2023-09-16 05:02:22 -0500

    Update Log.java

--- src/main/java/me/Visionexe/ZombieArena/Log.java
+++ src/main/java/me/Visionexe/ZombieArena/Log.java
@@ -3,7 +3,6 @@ package me.Visionexe.ZombieArena;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Zombie;
 
 import java.util.UUID;
 
