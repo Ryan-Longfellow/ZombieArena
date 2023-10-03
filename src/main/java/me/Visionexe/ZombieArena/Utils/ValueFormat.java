7fb23356f1f8ef6be1555b551afc4f28bdd0aad9
Ryan-Longfellow <30578561+Ryan-Longfellow@users.noreply.github.com>
2023-10-03 17:05:40 -0500

    Fix formatter

--- src/main/java/me/Visionexe/ZombieArena/Utils/ValueFormat.java
+++ src/main/java/me/Visionexe/ZombieArena/Utils/ValueFormat.java
@@ -4,13 +4,13 @@ public class ValueFormat {
     public static String format(long value) {
         // Trillions
         if (value >= 1000000000000.0) {
-            return (((Math.round(value / 100000000.0)) / 10000.0) + "T").replace(".0", "");
+            return (((Math.round(value / 100000000.0)) / 10000.0) + "T");
         } else if (value >= 1000000000.0) { // Billions
-            return (((Math.round(value / 1000000.0)) / 1000.0) + "B").replace(".0", "");
+            return (((Math.round(value / 1000000.0)) / 1000.0) + "B");
         } else if (value >= 1000000.0) { // Millions
-            return (((Math.round(value / 10000.0)) / 100.0) + "M").replace(".0", "");
+            return (((Math.round(value / 10000.0)) / 100.0) + "M");
         } else if (value >= 1000) { // Thousands
-            return (((Math.round(value / 100.0)) / 10.0) + "K").replace(".0", "");
+            return (((Math.round(value / 100.0)) / 10.0) + "K");
         }
         return String.valueOf(value);
     }
